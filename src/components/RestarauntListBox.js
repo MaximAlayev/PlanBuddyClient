6fea0a3ac408fd6f7768f94fa98fcbda9eb26a94
Maxim Alayev <neurologicalstyle@gmail.com>
2021-03-18 19:54:36 -0700

    Web format changes

--- src/components/RestarauntListBox.js
+++ src/components/RestarauntListBox.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Button, Form, Col } from 'react-bootstrap';
+import { Button, Container, Form, Row, Col } from 'react-bootstrap';
 import Axios from 'axios';
 import Fade from 'react-reveal/Fade';
 import './RestarauntListBox.css'
 
 //const API_ID = 'http://localhost:5000'
 const API_ID = 'https://plan-buddy.herokuapp.com'
+
 const RestarauntListBox = (props) => {
   const [pollId, setPollId] = useState('');
   const [userId, setUserId] = useState('');
@@ -15,7 +16,7 @@ const RestarauntListBox = (props) => {
 
   useEffect(() => {
     if (document.cookie === '') {
-      document.cookie =`user_id=${Math.random().toString(36).substr(2, 9)}`
+      document.cookie = `user_id=${Math.random().toString(36).substr(2, 9)}`
     }
     console.log(document.cookie)
     // Eventually ensure that this id is unique otherwise people screw over eachother's polls
@@ -26,40 +27,40 @@ const RestarauntListBox = (props) => {
     var tempUserId = document.cookie.split('; ').find(row => row.startsWith('user_id=')).split('=')[1]
     setUserId(tempUserId)
     setPollId(sessions_poll_id)
-    
+
     //https://cors-anywhere.herokuapp.com/
     Axios.get(`${API_ID}/api/get-restaraunt-list/${sessions_poll_id}/${tempUserId}`)
-    .then((response) => {
-      console.log(response.data)
-      setRestarauntList(response.data)
-      updateUpvoteDict(sessions_poll_id, tempUserId)
-    })
-    .catch((err) => {
+      .then((response) => {
+        console.log(response.data)
+        setRestarauntList(response.data)
+        updateUpvoteDict(sessions_poll_id, tempUserId)
+      })
+      .catch((err) => {
         console.log(err)
-    })
+      })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const updateRestarauntList = () => {
     Axios.get(`${API_ID}/api/get-restaraunt-list/${pollId}/${userId}`)
-    .then((response) => {
-      console.log(upvoteDict)
-      console.log(response.data)
-      setRestarauntList(response.data)
-    })
-    .catch((err) => {
+      .then((response) => {
+        console.log(upvoteDict)
+        console.log(response.data)
+        setRestarauntList(response.data)
+      })
+      .catch((err) => {
         console.log(err)
-    })
+      })
   };
 
   const updateUpvoteDict = (pollId, userId) => {
     Axios.get(`${API_ID}/api/get-upvote-dict/${pollId}/${userId}`)
-    .then((response) => {
-      setUpvoteDict(response.data)
-    })
-    .catch((err) => {
+      .then((response) => {
+        setUpvoteDict(response.data)
+      })
+      .catch((err) => {
         console.log(err)
-    })
+      })
   };
 
 
@@ -69,14 +70,14 @@ const RestarauntListBox = (props) => {
       userId: userId,
       restarauntName: restarauntName
     })
-    .then((response) => {
-      updateRestarauntList()
-      updateUpvoteDict(pollId, userId)
-      setAddPrompt(false)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+      .then((response) => {
+        updateRestarauntList()
+        updateUpvoteDict(pollId, userId)
+        setAddPrompt(false)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   };
 
   const upvoteRestaraunt = (restarauntName) => {
@@ -85,13 +86,13 @@ const RestarauntListBox = (props) => {
       userId: userId,
       restarauntName: restarauntName
     })
-    .then((response) => {
-      updateRestarauntList()
-      updateUpvoteDict(pollId, userId)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+      .then((response) => {
+        updateRestarauntList()
+        updateUpvoteDict(pollId, userId)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   };
 
   const downvoteRestaraunt = (restarauntName) => {
@@ -100,13 +101,13 @@ const RestarauntListBox = (props) => {
       userId: userId,
       restarauntName: restarauntName
     })
-    .then((response) => {
-      updateRestarauntList()
-      updateUpvoteDict(pollId, userId)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+      .then((response) => {
+        updateRestarauntList()
+        updateUpvoteDict(pollId, userId)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   };
 
   function AddSpecialRestaraunt() {
@@ -114,23 +115,27 @@ const RestarauntListBox = (props) => {
     return (
       <>
         <Fade clear appear when={!addPrompt} collapse>
-          <Button style={{margin: "2rem"}} className = "unvote-btn" onClick = {() => {setAddPrompt(true)}}>
+          <Button size='sm' style={{ margin: "2rem" }} className="unvote-btn" onClick={() => { setAddPrompt(true) }}>
             Add a new one
           </Button>
         </Fade>
         <Fade bottom appear when={addPrompt} collapse>
-            <h1>Add a New Option</h1>
-            <Form.Row style = {{margin: "3rem"}} className="align-items-center">
-                <Form.Label>Location Name</Form.Label>
-                <Form.Control type="locationname" placeholder="Enter Location Name"
-                onChange={(e)=>{newLocationName = e.target.value}}/>
-            </Form.Row>
-            <Button style = {{margin: "1rem"}} className="unvote-btn" onClick = { () => {
-              if (newLocationName !== '') {
-                addNewRestaraunt(newLocationName)
-              }
-            }}>
-              Submit
+          <h1 style={{ fontSize: "1.5rem" }}>
+            Add a New Option
+            </h1>
+          <Form.Row style={{ margin: "1 rem" }} className="align-items-center smaller-text">
+            <Form.Label className='smaller-text'> 
+              Location Name
+            </Form.Label>
+            <Form.Control type="locationname" placeholder="Enter Location Name"
+              onChange={(e) => { newLocationName = e.target.value }} />
+          </Form.Row>
+          <Button size='sm' style={{ margin: "1rem" }} className="unvote-btn" onClick={() => {
+            if (newLocationName !== '') {
+              addNewRestaraunt(newLocationName)
+            }
+          }}>
+            Submit
             </Button>
         </Fade>
       </>
@@ -139,35 +144,35 @@ const RestarauntListBox = (props) => {
 
   return (
     <>
-        <div className="card-body list-wrapper">
-        <a href= {`https://planbuddy.alayev.com/${pollId}`}>https://planbuddy.alayev.com/{pollId}</a>
+      <div className="card-body list-wrapper">
+        <a style={{ fontSize: '.9 rem' }} href={`https://planbuddy.alayev.com/${pollId}`}>https://planbuddy.alayev.com/{pollId}</a>
         <ul>
-          <Form style={{margin: "2rem"}}>
+          <Container fluid style={{ margin: "2rem" }}>
             {restarauntList.map((value) => {
               return (
                 <Fade bottom>
-                  <Form.Row>
-                    <Col xs={8} className="restaraunt-text">
+                  <Row>
+                    <Col className="restaraunt-text">
                       {value[0]}
                     </Col>
                     <Col className="mb-2">
                       <Button variant="primary"
-                      style={{margin: ".3 rem"}}
-                      className = {!upvoteDict[value[0]] ? "unvote-btn" : ""}
-                      onClick = {() => {
-                        !upvoteDict[value[0]] ? upvoteRestaraunt(value[0]) : downvoteRestaraunt(value[0])
-                      }}>
+                        size='sm'
+                        className={!upvoteDict[value[0]] ? "unvote-btn top-margin" : "top-margin"}
+                        onClick={() => {
+                          !upvoteDict[value[0]] ? upvoteRestaraunt(value[0]) : downvoteRestaraunt(value[0])
+                        }}>
                         Upvotes: {value[1]}
                       </Button>
                     </Col>
-                  </Form.Row>
+                  </Row>
                 </Fade>
               )
             })}
-          </Form>
-          <AddSpecialRestaraunt/>
+          </Container>
+          <AddSpecialRestaraunt />
         </ul>
-        </div>
+      </div>
 
     </>
   );
